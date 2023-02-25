7c81d1b6f43311397186538c871862c20d839dbf
fredrikjern <fjernberg@gmail.com>
2023-02-25 20:16:05 +0100

    comments, some styling, cleaned the code a bit.

--- main.js
+++ main.js
@@ -1,70 +1,70 @@
-const UPDATE_INTERVAL = 1000;
-const GAME_LIMIT = 12;
+const UPDATE_INTERVAL = 700;
+const GAME_LIMIT = 50;
 const API_BASE = "https://nackademin-item-tracker.herokuapp.com/";
-let iterations = 0;
-let deleteCurrentGame = document.querySelector(".delete-current-game");
-let gameOverBtn = document.querySelector(".game-over-button");
-let showIdContainer = document.querySelector(".showID");
-let deleteForm = document.querySelector(".delete-form");
-let deleteInput = document.querySelector(".delete-input");
+
 let gameContainer = document.querySelector(".game-container");
+let showIdContainer = document.querySelector(".showID");
 let startGameBtn = document.querySelector(".start");
 let joinForm = document.querySelector(".join-form");
+let signals = document.querySelectorAll(".signal");
 let initial = document.querySelector(".initial");
 let p1div = document.querySelector(".p1");
 let p2div = document.querySelector(".p2");
-let signals = document.querySelectorAll(".signal");
 
-let game;
-let lastRenderTime = 0;
-let player1 = null;
-let gameOver = false;
-let gameName;
-let gameID;
-let player1turn;
-let board = [];
 const correctArrays = [
-  [0, 1, 2],
+  [0, 1, 2], //Horisontella
   [3, 4, 5],
   [6, 7, 8],
-  [0, 3, 6],
+  [0, 3, 6], // Vertikala
   [1, 4, 7],
   [2, 5, 8],
-  [0, 4, 8],
+  [0, 4, 8], //Diagonaler
   [2, 4, 6],
 ];
+let gameOver = false;
+let player1 = null;
+let iterations = 0;
+let board = [];
 
+/** gameLoop()
+ * Incursive function, if gameOver = false it sets a timeout to call itself UPDATE_INTERVAL ms later
+ * then executes all the game logic and visual changes.
+ * @param {* String , the database ID} gameID
+ */
 async function gameLoop(gameID) {
   let game = await fetchGame(gameID);
   board = game.board;
   gameOver = checkWinner(board, correctArrays);
+  renderBoard(board);
 
   if (!gameOver && iterations < GAME_LIMIT) {
+    boardEventListeners(board, game.player1turn, player1, gameID);
     setTimeout(() => {
       gameLoop(gameID);
     }, UPDATE_INTERVAL);
   } else {
-    console.log("game over!   iterations: " + iterations);
+    console.log("game over! iterations: " + iterations);
     setTimeout(() => {
       deleteGame(gameID);
-    }, 3000);
+    }, 2000);
     renderBoard(board);
   }
-
-  renderBoard(board);
-
   if (game.player2joined) p2div.classList.remove("play2not");
   if (game.player1turn) {
-    signals[0].classList.add("active");
-    signals[1].classList.remove("active");
+    // Stylingen borde in i en egen funktion
+    if (!signals[0].classList.contains("active"))
+      signals[0].classList.add("active");
+    if (signals[1].classList.contains("active"))
+      signals[1].classList.remove("active");
   } else if (!game.player1turn) {
-    signals[1].classList.add("active");
-    signals[0].classList.remove("active");
+    if (!signals[1].classList.contains("active"))
+      signals[1].classList.add("active");
+    if (signals[0].classList.contains("active"))
+      signals[0].classList.remove("active");
   }
-  boardEventListeners(board, game.player1turn, player1, gameID);
+  iterations++;
 }
-// ---- Färdiga
-//
+// ! ----  Functions --------
 function checkWinner(board, correctArrays) {
   let winner = null;
   let winningArray = [];
@@ -113,11 +113,17 @@ async function player2join(gameID) {
     }),
   });
 }
-//
+/** checkSquare
+ *  checkSquare sends the updated board
+ * @param {* Array - Current gameboard} board
+ * @param {* Boolean - Who just played} player1turn
+ * @param {* String - databaseID} gameID
+ * @returns
+ */
 async function checkSquare(board, player1turn, gameID) {
   try {
     let turn = player1turn ? false : true;
-    let res = await fetch(`${API_BASE}lists/${gameID}`, {
+    await fetch(`${API_BASE}lists/${gameID}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -127,56 +133,40 @@ async function checkSquare(board, player1turn, gameID) {
         player1turn: turn,
       }),
     });
-    let data = await res.json();
-    return data;
   } catch (error) {
     console.log(error);
   }
 }
-//
 function boardEventListeners(board, player1turn, player1, gameID) {
   let squares = document.querySelectorAll(".square");
-
   squares.forEach((square, index) => {
-    // console.log(player1);
-    // console.log(player1turn);
     if (player1 && player1turn && board[index] === 0) {
-      console.log("kör denna");
+      // OM Du är spelare 1 OCH det är spelare 1s tur OCH ingen har checkat den rutan --> Lägg till eventlistener
       square.addEventListener("click", async (event) => {
         event.preventDefault();
-        square.innerHTML = "X";
-        board[index] = 1;
-        console.log(board);
-        console.log("klick: " + index);
-        renderBoard(board);
-        checkSquare(board, player1turn, gameID);
-        //
+        board[index] = 1; // Uppdatera boarden i rutan som klickas på till 1 (player1) (muterar)
+        renderBoard(board); // Renderar om för att få bort alla eventlisteners, så man inte kan dubbelklicka
+        checkSquare(board, player1turn, gameID); // Skickar en PUT med den muterade boarden, och byter vems tur det är
       });
     }
     if (!player1 && !player1turn && board[index] === 0) {
       square.addEventListener("click", (event) => {
-        console.log("player 2 fick eventlisteners");
         event.preventDefault();
-        square.innerHTML = "O";
         board[index] = 2;
-        console.log(board);
         renderBoard(board);
         checkSquare(board, player1turn, gameID);
-        console.log("klick: " + index);
       });
     }
   });
 }
-/**
- * This functions creates the div's that become the squares and marks them with X/O
- * @param {* An array []} board
+/** renderBoard()
+ * This functions creates the div's that become the squares and sets innerHTML to an SVG with X or O
+ * @param {* An array [0,0,0,0,0,0,0,0,0]} board
  */
 function renderBoard(board) {
-  console.log(board);
   gameContainer.innerHTML = "";
-  board.forEach((squareresults, index) => {
+  board.forEach((squareresults) => {
     let square = document.createElement("div");
-    //square.innerHTML=`${squareresults}`
     square.classList.add("square");
     if (squareresults === 1)
       square.innerHTML =
@@ -233,7 +223,6 @@ async function findGameID(gameName) {
     let res = await fetch(`${API_BASE}listsearch?listname=${gameName}`);
     let data = await res.json();
     let g = await data[0]._id;
-    console.log(g);
     return g;
   } catch (error) {
     console.log(error);
@@ -262,7 +251,7 @@ function randomNumberStr() {
 startGameBtn.addEventListener("click", async function (e) {
   e.preventDefault();
   player1 = true;
-  gameName = randomNumberStr();
+  let gameName = randomNumberStr();
   let gameID = await createGame(gameName);
   showIdContainer.innerHTML = `${gameName} (copied to clipboard)`;
   copyToClipboard(gameName);
@@ -274,17 +263,9 @@ joinForm.addEventListener("submit", async (event) => {
   //player1 = true;
   player1 = false;
   let joinInput = document.querySelector(".join-input").value;
-
   showIdContainer.innerHTML = `${joinInput} (copied to clipboard)`;
   initial.classList.add("hidden");
-  gameID = await findGameID(joinInput);
-  let resp = await player2join(gameID);
-  console.log(resp);
+  let gameID = await findGameID(joinInput);
+  await player2join(gameID);
   gameLoop(gameID);
 });
-//! ---
-
-//!  -------
-// let array = [0, 1, 2, 3, 4, 5, 7, 8]
-
-// array.forEach((item,index) => console.log(item + " " + index));
